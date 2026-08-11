8b2933b3a61dab421fd6e47e2c8ff14b8cd51c5f
YesnielX <yesnielviv2@gmail.com>
2026-08-11 13:14:21 -0400

    Correccion de identidad del proyecto (AlejandriaHQ)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'AlenjadriaHQ',
+  appId: 'com.alejandriahq.alejandria',
+  appName: 'AlejandriaHQ',
   webDir: 'www'
 };
 
