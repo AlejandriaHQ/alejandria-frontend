9b45908b504a4e58d67347202794de4cac0f8fdc
Dionel Familia Barroso <Dionelfamilia23@gmail.com>
2026-08-10 23:27:46 -0400

    inicializacion de catalogo, creacion de carpetas y diseño

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -7,7 +7,11 @@ const routes: Routes = [
   {
     path: '',
     component: AdminPage
+  },  {
+    path: 'catalogo',
+    loadChildren: () => import('./catalogo/catalogo.module').then( m => m.CatalogoPageModule)
   }
+
 ];
 
 @NgModule({
